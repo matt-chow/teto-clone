7e938dba4256766de19dc4466e0323bb4307a525
Chowtown <blux2703@gmail.com>
2025-09-19 18:06:47 -0400

    feat: starter Tetris app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#121212",
+        color: "#fff",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Hello, Tetris!</h1>
+      <p>Starting Tetris project</p>
+      <p>Edit <code>src/App.jsx</code> to get started.</p>
+    </div>
+  );
+}
+
+export default App;
